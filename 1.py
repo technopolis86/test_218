06b83de9e13e159d1880a22a929c8a2535619f73
М. С. <41207887+technopolis86@users.noreply.github.com>
2024-10-11 15:20:32 +0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -3,7 +3,7 @@ def main():
     print('Hello, GIT!')
     print('Hello, group 218!!!!')
     print(123)
-    print('456789')
+    print('СЕгодня 11 октября 2024 года')
 
 
 if __name__ == '__main__':
